3ce532c9d33f374df83d6d396050f224cb8cbf3e
chiranshu khandelwal <chiranshumain2gmail.com>
2023-06-29 01:17:36 +0530

    feat: chabot initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import ChatBot from "react-simple-chatbot";
+import { useState } from "react";
 
-function App() {
+const CustomDropdown = (props) => {
+  const { triggerNextStep } = props;
+  const [selectedAge, setSelectedAge] = useState(null);
+
+  const handleOptionClick = (value) => {
+    setSelectedAge(value);
+    triggerNextStep({ value });
+  };
+
+  const options = [];
+  for (let age = 18; age <= 40; age++) {
+    options.push({ value: String(age), label: String(age) });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ maxHeight: "100px", overflowY: "auto", maxWidth: "30px"}}>
+      <p>Enter Your Age: </p>
+      {options.map((option) => (
+        <div
+          key={option.value}
+          onClick={() => handleOptionClick(option.value)}
+          style={{
+            fontWeight: option.value === selectedAge ? "bold" : "normal",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {option.label}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+const Chatbot = () => {
+  const steps2 = [
+    {
+      id: "initial_msg",
+      message: "Hello, Welcome to student info system!",
+      trigger: "ask_response",
+    },
+    {
+      id: "ask_response",
+      options: [
+        {
+          value: "got_it",
+          label: "Got it",
+          trigger: "ask_name",
+        },
+      ],
+    },
+    {
+      id: "ask_name",
+      message: "Enter your Name",
+      trigger: "get_name",
+    },
+    {
+      id: "get_name",
+      user: true,
+      trigger: "ask_age",
+    },
+    {
+      id: "ask_age",
+      component: <CustomDropdown />,
+      waitAction: true,
+      trigger: "print_age",
+    },
+    {
+      id: 'print_age',
+      message: '{previousValue}',
+      trigger: 'end_message',
+    },
+    {
+      id: "end_message",
+      message: "Thanks! Your data was submitted successfully!",
+      end: true,
+    },
+  ];
+
+  return (
+    <div className="App">
+      <ChatBot steps={steps2} />
+    </div>
+  );
+};
+
+export default Chatbot;
+
+
+
+
