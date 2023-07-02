0d0a566ae0db503009314447b9ff1a9e07d0e4f5
chiranshu khandelwal <chiranshumain2gmail.com>
2023-07-02 16:23:45 +0530

    fix: linting

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Details = ({name, age}) => {
+const Details = ({ name, age }) => {
   return (
     <div>
       <p>
-        Your name {name} aged {age} has been added to student system. You may now
-        exit.
+        Your name {name} aged {age} has been added to student system. You may
+        now exit.
       </p>
     </div>
   );
